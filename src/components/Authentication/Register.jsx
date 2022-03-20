2cfa1debc8f8701f22b8b747e3d6fbe42eebe9e2
Shreyas Soni <sonishreyas10@gmail.com>
2022-03-21 02:47:56 +0530

    ref: reducer fixed

--- src/components/Authentication/Register.jsx
+++ src/components/Authentication/Register.jsx
@@ -1,4 +1,5 @@
 import { useLocation, useNavigate } from "react-router";
+import { useState } from "react";
 import { useRegister } from "../../context";
 import { registerHandler } from "../../utils";
 
@@ -6,51 +7,51 @@ const Register = () => {
     const { registerState, registerDispatch } = useRegister();
     const location = useLocation();
     const navigate = useNavigate();
+    const [showPassword,setShowPassword] = useState();
+    const [showConfirmPassword, setShowConfirmPassword] = useState();
 
-    const setValueHandler = (e,field) => {
+    const setValueHandler = (e,field,type) => {
         const fieldValue = {};
         fieldValue[field] = e.target.value;
+        fieldValue["type"] = type;
         registerDispatch(fieldValue);
     }
-    const setFocusHandler = (field, value) => {
+
+    const setFocusHandler = (field, value, type) => {
         const focusReset = { firstName: false, lastName: false,	email: false, password: false, confirmPassword: false }
         focusReset[field] = value;
-        registerDispatch({focus: focusReset});
+        registerDispatch({focus: focusReset, type: type});
     }
-    const showPasswordHandler = (field) => {
-        const showPassword = {...registerState.showPassword};
-        if(showPassword[field])
-            showPassword[field] = false;
-        else
-        showPassword[field] = true;
-        registerDispatch({showPassword: showPassword});
-    }
+
+    const showPasswordHandler = () => showPassword ? setShowPassword(false) : setShowPassword(true);
+
+    const showConfirmPasswordHandler = () => showConfirmPassword ? setShowConfirmPassword(false) : setShowConfirmPassword(true);
 
     return (
         <form onSubmit={(e) => registerHandler(e, location, navigate)} className="input-form register flex-column flex-gap-1 flex-wrap h-auto w-100">
             <section className={`input-container flex-column m-5 ${registerState.firstName.length > 0 || registerState.focus.firstName ? "focused":"" }`}>
-                <input id="first-name" className="textbox-content p-5" type="text" name="first-name" onChange={(e) => setValueHandler(e,"firstName")} value={registerState.firstName} onFocus={() => setFocusHandler("firstName",true)} onBlur={()=> setFocusHandler("firstName",false)}/>
+                <input id="first-name" className="textbox-content p-5" type="text" name="first-name" onChange={(e) => setValueHandler(e, "firstName", "UPDATE_FIRST_NAME")} value={registerState.firstName} onFocus={() => setFocusHandler("firstName", true, "UPDATE_FOCUS")} onBlur={()=> setFocusHandler("firstName", false, "UPDATE_FOCUS")}/>
                 <label htmlFor="first-name" className="textbox-label m-0 px-1"> First Name<span className="required-field">*</span>
                 </label>
                 <sub className="p-2 my-2 inactive-check"></sub>
             </section>
 
             <section className={`input-container flex-column m-5 ${registerState.lastName.length > 0 || registerState.focus.lastName ? "focused":"" }`}>
-                <input id="last-name" className="textbox-content p-5" type="text" name="last-name" onChange={(e) => setValueHandler(e,"lastName")} value={registerState.lastName} onFocus={() => setFocusHandler("lastName",true)} onBlur={()=> setFocusHandler("lastName",false)}/>
+                <input id="last-name" className="textbox-content p-5" type="text" name="last-name" onChange={(e) => setValueHandler(e, "lastName", "UPDATE_LAST_NAME")} value={registerState.lastName} onFocus={() => setFocusHandler("lastName", true, "UPDATE_FOCUS")} onBlur={()=> setFocusHandler("lastName", false, "UPDATE_FOCUS")}/>
                 <label htmlFor="last-name" className="textbox-label m-0 px-1"> Last Name<span className="required-field">*</span>
                 </label>
                 <sub className="p-2 my-2 inactive-check"></sub>
             </section>
 
             <section className={`input-container flex-column m-5 ${registerState.email.length > 0 || registerState.focus.email ? "focused":"" }`}>
-                <input id="email" className="textbox-content p-5" type="email" name="email" onChange={(e) => setValueHandler(e,"email")} value={registerState.email} onFocus={() => setFocusHandler("email",true)} onBlur={()=> setFocusHandler("email",false)}/>
+                <input id="email" className="textbox-content p-5" type="email" name="email" onChange={(e) => setValueHandler(e, "email", "UPDATE_EMAIL")} value={registerState.email} onFocus={() => setFocusHandler("email", true, "UPDATE_FOCUS")} onBlur={()=> setFocusHandler("email", false, "UPDATE_FOCUS")}/>
                 <label htmlFor="email" className="textbox-label m-0 px-1"> Email<span className="required-field">*</span>
                 </label>
                 <sub className="p-2 my-2 inactive-check"></sub>
             </section>
 
             <section className={`input-container flex-column m-5 ${registerState.password.length > 0 || registerState.focus.password ? "focused":"" }`}>
-                <input id="password" className="textbox-content p-5" type={`${ registerState.showPassword.password ? "text" : "password" }`} name="password" onChange={(e) => setValueHandler(e,"password")} value={registerState.password} onFocus={() => setFocusHandler("password",true)} onBlur={()=> setFocusHandler("password",false)}/>
+                <input id="password" className="textbox-content p-5" type={`${ registerState.showPassword.password ? "text" : "password" }`} name="password" onChange={(e) => setValueHandler(e, "password", "UPDATE_PASSWORD")} value={registerState.password} onFocus={() => setFocusHandler("password", true, "UPDATE_FOCUS")} onBlur={()=> setFocusHandler("password", false, "UPDATE_FOCUS")}/>
                 <i className={`fas ${ registerState.showPassword.password ? "fa-eye-slash" : "fa-eye" } show-password`} id="show-password"
                     onClick={ () => showPasswordHandler("password") }
                 ></i>
@@ -60,7 +61,7 @@ const Register = () => {
             </section>
 
             <section className={`input-container flex-column m-5 ${registerState.confirmPassword.length > 0 || registerState.focus.confirmPassword ? "focused":"" }`}>
-                <input id="confirm-password" className="textbox-content p-5" type={`${ registerState.showPassword.confirmPassword ? "text" : "password" }`} name="confirmPassword" onChange={(e) => setValueHandler(e,"confirmPassword")} value={registerState.confirmPassword} onFocus={() => setFocusHandler("confirmPassword",true)} onBlur={()=> setFocusHandler("confirmPassword",false)}/>
+                <input id="confirm-password" className="textbox-content p-5" type={`${ registerState.showPassword.confirmPassword ? "text" : "password" }`} name="confirmPassword" onChange={(e) => setValueHandler(e, "confirmPassword", "UPDATE_CONFIRM_PASSWORD")} value={registerState.confirmPassword} onFocus={() => setFocusHandler("confirmPassword", true, "UPDATE_FOCUS")} onBlur={()=> setFocusHandler("confirmPassword", false, "UPDATE_FOCUS")}/>
                 <i className={`fas ${ registerState.showPassword.confirmPassword ? "fa-eye-slash" : "fa-eye" } show-password`} id="show-password"
                     onClick={ () => showPasswordHandler("confirmPassword") }
                 ></i>
--- src/reducers/auth-reducer.js
+++ src/reducers/auth-reducer.js
@@ -5,7 +5,25 @@
  * @returns {Object} Updated loginState
  */
 const loginReducer = (loginState, loginAction) => {
-	return { ...loginState, ...loginAction };
+	switch (loginAction.type) {
+		case "UPDATE_EMAIL":
+			return { ...loginState, email: loginAction.email };
+		case "UPDATE_PASSWORD":
+			return {
+				...loginState,
+				password: loginAction.password,
+			};
+		case "UPDATE_FOCUS":
+			return { ...loginState, focus: loginAction.focus };
+		case "TEST_CREDENTIAL":
+			return {
+				...loginState,
+				email: loginAction.email,
+				password: loginAction.password,
+			};
+		default:
+			return loginState;
+	}
 };
 
 /**
@@ -15,7 +33,29 @@ const loginReducer = (loginState, loginAction) => {
  * @returns {Object} Updated registerState
  */
 const registerReducer = (registerState, registerAction) => {
-	return { ...registerState, ...registerAction };
+	switch (registerAction.type) {
+		case "UPDATE_EMAIL":
+			return { ...registerState, email: registerAction.email };
+		case "UPDATE_FIRST_NAME":
+			return { ...registerState, firstName: registerAction.firstName };
+		case "UPDATE_LAST_NAME":
+			return { ...registerState, lastName: registerAction.lastName };
+		case "UPDATE_PASSWORD":
+			return {
+				...registerState,
+				password: registerAction.password,
+			};
+		case "UPDATE_CONFIRM_PASSWORD":
+			return {
+				...registerState,
+				confirmPassword: registerAction.confirmPassword,
+			};
+		case "UPDATE_FOCUS":
+			return { ...registerState, focus: registerAction.focus };
+
+		default:
+			return registerState;
+	}
 };
 
 export { loginReducer, registerReducer };
