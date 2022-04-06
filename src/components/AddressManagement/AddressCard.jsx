af1b78ae64b8acae244b5be661e262ca80563a96
Shreyas Soni <sonishreyas10@gmail.com>
2022-04-06 16:26:31 +0530

    ref: address component refactored

--- src/components/AddressManagement/AddressCard.jsx
+++ src/components/AddressManagement/AddressCard.jsx
@@ -44,24 +44,24 @@ const AddressCard = ({ props }) => {
 		removeFromAddressHandler(e, addressId, addressDispatch, addressState);
 	};
 	return (
-		<div className="flex-column justify-content-center flex-wrap p-10 b-radius-2 w-100 h-auto">
-			<h3 className="name p-2">{name}</h3>
-			<p className="address p-2">
+		<div className="flex-column justify-content-center flex-wrap b-radius-2 w-100 h-auto">
+			<h3 className="p-2">{name}</h3>
+			<p className="p-2">
 				{houseNo} , {society}, {area}
 			</p>
-			<p className="city p-2">{city}</p>
-			<p className="state p-2">{state}</p>
-			<p className="country p-2">{country}</p>
-			<p className="pincode p-2">{pincode}</p>
+			<p className="p-2">{city}</p>
+			<p className="p-2">{state}</p>
+			<p className="p-2">{country}</p>
+			<p className="p-2">{pincode}</p>
 			<section className="card-footer flex-row flex-grow-1 justify-content-center flex-gap-1 py-5 px-0 ">
 				<button
-					className="primary-btn edit-btn p-5 b-radius-2 mx-5 my-0 text-bold flex-grow-1 w-100 h5"
+					className="primary-btn edit-btn p-2 b-radius-2 my-0 text-bold flex-grow-1 w-100 h5"
 					onClick={handleEditAddress}
 				>
 					Edit
 				</button>
 				<button
-					className="outline-btn delete-btn p-5 b-radius-2 mx-5 my-0 text-bold flex-grow-1 w-100 h5"
+					className="outline-btn delete-btn p-2 b-radius-2 my-0 text-bold flex-grow-1 w-100 h5"
 					onClick={handleDeleteAddress}
 				>
 					Delete
--- src/components/Cart/CartContent.jsx
+++ src/components/Cart/CartContent.jsx
@@ -12,27 +12,30 @@ const CartContent = () => {
 	const { cartState } = useCart();
 	const { showAddressModal, showAddressFormModal } = useAddress();
 	return (
-		<main className="main flex-column justify-content-center align-center">
-			<h1 className="homepage-heading text-center m-5 p-5">My Cart</h1>
-			{cartState.cartData.length !== 0 ? (
-				<section className="rui-main--heading-container no-border">
-					<article className="cart-container grid-2-column">
-						<div className="cart-content flex-column justify-content-center flex-wrap">
-							<ul className="cart-list">
-								<Address />
-								{showAddressModal && <AddressModal />}
-								{showAddressFormModal && <AddressFormModal />}
-								<CartProducts />
-							</ul>
-						</div>
-						<Billing />
-					</article>
-				</section>
-			) : (
-				<h4 className="h4 text-center m-5 p-5">No items in Cart</h4>
-			)}
-			{/* <Coupon/> */}
-		</main>
+		<>
+			<main className="main flex-column justify-content-center align-center">
+				<h1 className="homepage-heading text-center m-5 p-5">My Cart</h1>
+				{cartState.cartData.length !== 0 ? (
+					<section className="rui-main--heading-container no-border">
+						<article className="cart-container grid-2-column">
+							<div className="cart-content flex-column justify-content-center flex-wrap">
+								<ul className="cart-list">
+									<Address />
+									<CartProducts />
+								</ul>
+							</div>
+							<Billing />
+						</article>
+					</section>
+				) : (
+					<h4 className="h4 text-center m-5 p-5">No items in Cart</h4>
+				)}
+
+				{/* <Coupon/> */}
+			</main>
+			{showAddressModal && <AddressModal />}
+			{showAddressFormModal && <AddressFormModal />}
+		</>
 	);
 };
 
